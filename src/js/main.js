e13c7634fa9b22b4d0f2ac22355d5d9769b9e624
Lvl3Mage <lvlthreemage@gmail.com>
2022-07-19 20:20:29 +0200

    Fixed ibg

--- src/js/main.js
+++ src/js/main.js
@@ -101,6 +101,7 @@ function ibg(){
 	if($(this).find('img').length>0){
 		$(this).find('img').first().css("width", 0);
 		$(this).find('img').first().css("height", 0);
+		$(this).find('img').first().css("display", "none");
 		$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
 	}
 	});
